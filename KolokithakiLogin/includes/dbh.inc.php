05814cbcd6d32112119cc16fee9ce6854e3a3a3e
Nikolas Kapetanidis <n_kapetanidis@outlook.com>
2021-06-02 16:52:28 +0300

    login system v01

---
+++ KolokithakiLogin/includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+//Creating Variables
+$serverName = "localhost";
+$dBUsername = "root"; //Default name
+$dBPassword = "";     //Default password empty
+$dBName = "kolokithakidB";
+
+$conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dBName );//Connection variable = built-in function
+if (!$conn){    //if this returns false -> error message
+ die("Connection failed: " . mysqli_connect_error()); //shows specific error we are encountering
+}
