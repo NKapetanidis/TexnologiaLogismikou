6a85a87d0568de37db1ca5ba2e8d3416c2e8553d
Nikolas Kapetanidis <n_kapetanidis@outlook.com>
2021-06-05 13:40:18 +0300

    LogInSystem ErrorHandlers,dB Functional

--- KolokithakiLogin/login.php
+++ KolokithakiLogin/login.php
@@ -4,7 +4,7 @@ include_once 'header.php'
 
 <section class="login-form">
 <h2>Login Up</h2>
-<form action="login.inc.php" method="post">
+<form action="includes/login.inc.php" method="post">
 <input type="text" name="name" placeholder="Username/Email">
 <input type="text" name="pwd" placeholder="Password ">
 <button type="submit" name="submit">Log in</button>
